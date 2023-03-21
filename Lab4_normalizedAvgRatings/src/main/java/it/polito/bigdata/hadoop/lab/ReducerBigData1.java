6097f4008a05bea537d3753777a8b9ab83d66366
Uberts94 <s292522@studenti.polito.it>
2023-03-21 17:23:11 +0100

    first job completed and tested

--- Lab4_normalizedAvgRatings/src/main/java/it/polito/bigdata/hadoop/lab/ReducerBigData1.java
+++ Lab4_normalizedAvgRatings/src/main/java/it/polito/bigdata/hadoop/lab/ReducerBigData1.java
@@ -1,8 +1,9 @@
 package it.polito.bigdata.hadoop.lab;
 
 import java.io.IOException;
+import java.util.HashMap;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -12,18 +13,37 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 /* Set the proper data types for the (key,value) pairs */
 class ReducerBigData1 extends Reducer<
-                Text,           // Input key type
-                IntWritable,    // Input value type
-                Text,           // Output key type
-                IntWritable> {  // Output value type
-    
+                Text,           // Input key : UserId
+                RatingWritable,    // Input value : RatingWritable containing UserId, Score, ProductId (redundancy)
+                NullWritable,           // Output key type
+                Text> {  // Output value : RatingWritable containing UserId, normalized score, ProductId (redundancy) 
+	
+	double sum;
+	int ratingsCounter;
+	
     @Override
     protected void reduce(
         Text key, // Input key type
-        Iterable<IntWritable> values, // Input value type
+        Iterable<RatingWritable> values, // Input value type
         Context context) throws IOException, InterruptedException {
-
-		/* Implement the reduce method */
     	
+    	HashMap<String, Double> map = new HashMap<String, Double>(); //HashMap<ProductId,Score>
+    	sum = 0;
+    	ratingsCounter = 0;
+    	
+    	for(RatingWritable value : values) {
+    		sum+=value.getScore();
+    		ratingsCounter++;
+    		map.put(value.getProduct(), value.getScore());
+    	}
+    	
+    	map.forEach((prod, rating) -> {
+    		try {
+				context.write(NullWritable.get(), new Text(prod+","+key+","+(rating-(sum/ratingsCounter))));
+			} catch (IOException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	});
     }
 }
