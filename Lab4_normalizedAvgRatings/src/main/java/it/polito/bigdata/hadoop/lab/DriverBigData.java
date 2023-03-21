550eccbbf64a56cd4ceca3b87bd8b34f2b8a4e4d
Uberts94 <s292522@studenti.polito.it>
2023-03-21 13:38:50 +0100

    setting the Driver

--- Lab4_normalizedAvgRatings/src/main/java/it/polito/bigdata/hadoop/lab/DriverBigData.java
+++ Lab4_normalizedAvgRatings/src/main/java/it/polito/bigdata/hadoop/lab/DriverBigData.java
@@ -3,12 +3,11 @@ package it.polito.bigdata.hadoop.lab;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -16,13 +15,9 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import it.polito.bigdata.hadoop.lab.DriverBigData;
-import it.polito.bigdata.hadoop.lab.MapperBigData1;
-import it.polito.bigdata.hadoop.lab.MapperBigData2;
-import it.polito.bigdata.hadoop.lab.ReducerBigData1;
-import it.polito.bigdata.hadoop.lab.ReducerBigData2;
 
 /**
- * MapReduce program
+ * MapReduce program Lab4 - normalized avarage rating
  */
 public class DriverBigData extends Configured implements Tool {
 
@@ -37,21 +32,16 @@ public class DriverBigData extends Configured implements Tool {
 		Job job = Job.getInstance(conf);
 
 		// Assign a name to the job
-		job.setJobName("Lab#4 - Ex.1 - step 1");
+		job.setJobName("Lab#4 - normalizing ratings - step 1");
 
-		/*
-		 * ********************************************************* 
-		 * Fill out the missing parts/update the content of this method
-		 ************************************************************
-		*/
 		Path inputPath;
 		Path outputDir;
 		int numberOfReducersJob1;
 
 		// Parse the parameters for the set up of the first job
 		numberOfReducersJob1 = Integer.parseInt(args[0]);
-		inputPath = new Path(args[1]);
-		outputDir = new Path(args[2]);
+		inputPath = new Path(args[1]); //input
+		outputDir = new Path(args[2]); //output1
 
 		// Set the path of the input file/folder for this first job
 		FileInputFormat.addInputPath(job, inputPath);
@@ -63,81 +53,82 @@ public class DriverBigData extends Configured implements Tool {
 		job.setJarByClass(DriverBigData.class);
 
 		// Set job input format
-		job.setInputFormatClass(...);
+		job.setInputFormatClass(TextInputFormat.class);
 
 		// Set job output format
-		job.setOutputFormatClass(...);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
 		// Set map class
 		job.setMapperClass(MapperBigData1.class);
 
 		// Set map output key and value classes
-		job.setMapOutputKeyClass(...);
-		job.setMapOutputValueClass(...);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(DoubleWritable.class);
 
 		// Set reduce class
 		job.setReducerClass(ReducerBigData1.class);
 
 		// Set reduce output key and value classes
-		job.setOutputKeyClass(...);
-		job.setOutputValueClass(...);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
 
 		// Set number of reducers
 		job.setNumReduceTasks(numberOfReducersJob1);
 
 		// Execute the first job and wait for completion
 		if (job.waitForCompletion(true) == true) {
-			// Set up the second job
-			Job job2 = Job.getInstance(conf);
-
-			// Assign a name to the second job
-			job2.setJobName("Lab#3 - Ex.1 - step 2");
-
-			/* */
-			// Change the following part of the code
-			Path outputDir2;
-			int numberOfReducersJob2;
-			outputDir2 = new Path(args[4]);
-
-			// Set path of the input file/folder for this second job
-			// The output of the first job is the input of this second job    
-			FileInputFormat.addInputPath(job2, outputDir);
-
-			// Set path of the output folder for this job
-			FileOutputFormat.setOutputPath(job2, outputDir2);
-
-			// Specify the class of the Driver for this job
-			job2.setJarByClass(DriverBigData.class);
-
-			// Set job input format
-			job2.setInputFormatClass(...);
-
-			// Set job output format
-			job2.setOutputFormatClass(...);
-
-			// Set map class
-			job2.setMapperClass(MapperBigData2.class);
-
-			// Set map output key and value classes
-			job2.setMapOutputKeyClass(...);
-			job2.setMapOutputValueClass(...);
-
-			// Set reduce class
-			job2.setReducerClass(ReducerBigData2.class);
-
-			// Set reduce output key and value classes
-			job2.setOutputKeyClass(...);
-			job2.setOutputValueClass(...);
-
-			// Set the number of reducers of the second job 
-			numberOfReducersJob2 = ..;
-			job2.setNumReduceTasks(numberOfReducersJob2);
-
-			// Execute the second job and wait for completion
-			if (job2.waitForCompletion(true) == true)
-				exitCode = 0;
-			else
-				exitCode = 1;
+//			// Set up the second job
+//			Job job2 = Job.getInstance(conf);
+//
+//			// Assign a name to the second job
+//			job2.setJobName("Lab#4 - normalized average rating - step 2");
+//
+//			/* */
+//			// Change the following part of the code
+//			Path outputDir2;
+//			int numberOfReducersJob2;
+//			outputDir2 = new Path(args[4]);
+//
+//			// Set path of the input file/folder for this second job
+//			// The output of the first job is the input of this second job    
+//			FileInputFormat.addInputPath(job2, outputDir);
+//
+//			// Set path of the output folder for this job
+//			FileOutputFormat.setOutputPath(job2, outputDir2);
+//
+//			// Specify the class of the Driver for this job
+//			job2.setJarByClass(DriverBigData.class);
+//
+//			// Set job input format
+//			job2.setInputFormatClass(KeyValueTextInputFormat.class);
+//
+//			// Set job output format
+//			job2.setOutputFormatClass(TextOutputFormat.class);
+//
+//			// Set map class
+//			job2.setMapperClass(MapperBigData2.class);
+//
+//			// Set map output key and value classes
+//			job2.setMapOutputKeyClass(Text.class);
+//			job2.setMapOutputValueClass(DoubleWritable.class);
+//
+//			// Set reduce class
+//			job2.setReducerClass(ReducerBigData2.class);
+//
+//			// Set reduce output key and value classes
+//			job2.setOutputKeyClass(Text.class);
+//			job2.setOutputValueClass(DoubleWritable.class);
+//
+//			// Set the number of reducers of the second job 
+//			numberOfReducersJob2 = 1;
+//			job2.setNumReduceTasks(numberOfReducersJob2);
+//
+//			// Execute the second job and wait for completion
+//			if (job2.waitForCompletion(true) == true)
+//				exitCode = 0;
+//			else
+//				exitCode = 1;
+			exitCode = 0;
 		} else
 			exitCode = 1;
 
